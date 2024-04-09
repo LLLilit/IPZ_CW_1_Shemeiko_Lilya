86c76b7106499744e2a9edfb1b8059bcc2b4e495
LLLilit <144475735+LLLilit@users.noreply.github.com>
2024-04-09 16:29:57 +0300

    Update MainActivity.kt

--- app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
@@ -7,18 +7,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import ua.edu.lntu.ipz_cw_1_2.ui.theme.IPZ_CW_1_2_Shemeiko_LilyaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +48,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+    var text1 by remember { mutableStateOf(TextFieldValue()) }
+    var text2 by remember { mutableStateOf(TextFieldValue()) }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -58,10 +69,25 @@ fun Greeting() {
             modifier = Modifier
                 .padding(paddingValues)
         ) {
-
+            TextField(
+                value = text,
+                onValueChange = {text = it},
+                label = {Text("email")}
+            )
+            TextField(
+                value = text1,
+                onValueChange = {text1 = it},
+                label = {Text("password")}
+            )
+            Text(text = "Login success")
+            Button(onClick = { /*TODO*/ }) {
+                Text("Sing In")
+                
+            }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
